72986b72ea35976b1792b7c245b4041b23ffff44
Avani V Sheth <avs44@afsconnect2.njit.edu>
2017-10-22 13:09:36 -0400

    added

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ return '<h1>' . $msg1 . '</h1>';
 }
 static public function Format()
 {
-echo "<table cellpadding='6px' border='3px' style='border-collapse:inherit'>";
+echo "<table cellpadding='6px' border='3px' style='border-collapse:initial'>";
 }
 static public function Row()
 {
